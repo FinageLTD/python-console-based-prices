855cf80a88d4c8941203e19a3173c93ef51a9893
Alper San <alper@alpersan.dev>
2021-08-10 16:12:01 +0300

    Arrows added

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ from tabulate import tabulate
 from decimal import Decimal
 
 async def connect_to_Finage():
-    uri = "YOUR_SOCKET_ADDRESS"
+    uri = "YOUR_SERVER_ADDRESS"
     async with websockets.connect(uri) as websocket:
         symbols = input("Enter the symbols separated by commas: ")
         query = '{"action": "subscribe", "symbols": "%s"}' % symbols
@@ -23,24 +23,24 @@ async def connect_to_Finage():
                 dateList = []
                 changesList = []
 
-                price = jsonData['lp']
+                price = jsonData['a']
                 symbol = jsonData['s']
-                dailyChange = jsonData['cpd']
+                dailyChange = jsonData['dc']
                 timestamp = jsonData['t']
 
                 for key in prices.keys():
                     symbolList.append('\033[37m' + key)
                     priceList.append(
-                        '\033[32m' + prices[key][0] if price > prices[key][0] else '\033[31m' + prices[key][0])
+                        '\033[32m' + u'\u2b06 ' + str(prices[key][0]) if price > prices[key][0] else '\033[31m' + u'\u2b07 ' + str(prices[key][0]))
                     dateList.append('\033[94m' + datetime.utcfromtimestamp(
                         prices[key][2]/1000.0).strftime('%Y-%m-%d %H:%M:%S.%f'))
                     changesList.append('\033[32m' + prices[key][1] + "%" if Decimal(
-                        prices[key][1]) > 0 else '\033[31m' + prices[key][1] + "%")
+                        prices[key][1]) > 0 else '\033[31m' + prices[key][1])
 
                 prices[symbol] = [price, dailyChange, timestamp]
     
                 print("\033[2J\033[1;1H")
-                print(tabulate({"\033[33mSymbol": symbolList, "\033[33mPrice": priceList, "\033[33mDaily Change": changesList, "\033[33mDateTime": dateList}, headers="keys", numalign="right"))
+                print(tabulate({"\033[33mSymbol": symbolList, "\033[33mPrice": priceList, "\033[33mDaily Change (%)": changesList, "\033[33mDateTime": dateList}, headers="keys", numalign="right"))
 
             except Exception as e:
                 print(jsonData)
